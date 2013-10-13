f9ec74140673e7b62ad52b4790e2af09eecc63b6
Skylar Anderson <skylar@anderson.is>
2013-10-13 15:40:36 -0400

    moved ref logic to models

--- ember-data-firebase.js
+++ ember-data-firebase.js
@@ -4,14 +4,33 @@
 
 var fb;
 
-DS.FirebaseModel = DS.Model.extend({
-    init3: function() {
-        this._super();
-        this.on('didLoad', this._initLiveBindings.bind(this));
-        this.on('didCreate', this._initLiveBindings.bind(this));
-    },
-    getRef: function() {
+DS.FirebaseSerializer = DS.JSONSerializer.extend({
+    extractSingle: function() {
 
+    },
+    serializeBelongsTo: function(record, json, relationship) {
+        console.log("Serialize Belongs To");
+        return this._super(record, json, relationship);
+
+    },
+    serializeHasMany: function(record, json, relationship) {
+
+       var key = relationship.key,
+        relationshipType = DS.RelationshipChange.determineRelationshipType(record.constructor, relationship);
+
+        console.log("Serialize hasMany:");
+        console.log(key);
+        console.log(record);
+        console.log(relationshipType);
+        console.log("---");
+
+        return this._super(record, json, relationship);
+
+    }
+});
+
+DS.FirebaseModel = DS.Model.extend({
+    getRef: function() {
         var name = this.constructor.url,
             ref;
 
@@ -24,8 +43,36 @@ DS.FirebaseModel = DS.Model.extend({
 
         return ref;
 
+    }
+});
+
+DS.FirebaseLiveModel = DS.FirebaseModel.extend({
+    init: function() {
+        this._super();
+        this.on('didLoad', this._initLiveBindings.bind(this));
+        this.on('didCreate', this._initLiveBindings.bind(this));
     },
     _initLiveBindings: function() {
+
+        var ref = this.getRef(),
+            record = this;
+
+        ref.on("child_added", function() {
+            // This event will be triggered once for each initial child at this location,
+            // and it will be triggered again every time a new child is added. The
+            // DataSnapshot passed into the callback will reflect the data for the relevant
+            // child. For ordering purposes, it is passed a second argument which is a string
+            // containing the name of the previous sibling child by priority order (or null
+            // if it is the first child.)
+        });
+
+        ref.on("child_changed", function(snapshot) {
+            record.set(snapshot.name(), snapshot.val());
+        });
+
+        ref.on("child_removed", function(snapshot) {
+            record.set(snapshot.name(), null);
+        });
 
     },
     deleteRecord: function() {
@@ -61,7 +108,7 @@ DS.FirebaseAdapter = DS.Adapter.extend(Ember.Evented, {
         var ref = this._getRefForType(type).child(id);
 
         return new Ember.RSVP.Promise(function(resolve, reject) {
-            ref.on("value", function(snapshot) {
+            ref.once("value", function(snapshot) {
                 resolve(snapshot.val());
             }, function(error) { reject(error); });
         });
